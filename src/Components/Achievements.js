ba664f19b72d84a6971592c82a23f984ec2b7b51
Mohammed-Afeef-Sayeed <afeefsayeed4.4@gmail.com>
2023-06-13 14:56:42 +0530

    edited Acievements

--- src/Components/Achievements.js
+++ src/Components/Achievements.js
@@ -1,38 +1,37 @@
 import React, { Component } from 'react';
 
 class Achievements extends Component {
-  render() {
+   render() {
 
-    if(this.props.data){
-      var achievement = this.props.data.achievement.map(function(achievement){
-        return  <li key={achievement.user}>
-            <blockquote>
-               <p>{achievement.text}</p>
-               {/* <cite>{testimonials.user}</cite> */}
-            </blockquote>
-         </li>
-      })
-    }
+      if (this.props.data) {
+         var achievement = this.props.data.achievement.map(function (achievement) {
+            return <li key={achievement.user}>
+               <blockquote>
+                  <p>{achievement.text}</p>
+                  {/* <cite>{achievements.user}</cite> */}
+               </blockquote>
+            </li>
+         })
+      }
 
-    return (
-      <section id="achievements">
-      <div className="text-container">
-         <div className="row">
+      return (
+         <section id="achievements">
+            <div className="text-container">
+               <div className="row">
 
-            <div className="two columns header-col">
-               <h1><span>My Achievements</span></h1>
-            </div>
+                  <div className="two columns header-col">
+                     <h1><span>My Achievements</span></h1>
+                  </div>
 
-            <div className="ten columns flex-container">
-                  <ul className="slides">
-                      {achievement}
-                  </ul>
+                  <div className="ten columns flex-container">
+                     <ul className="slides"><li>{achievement}</li>
+                     </ul>
+                  </div>
                </div>
             </div>
-         </div>
-   </section>
-    );
-  }
+         </section>
+      );
+   }
 }
 
 export default Achievements;
